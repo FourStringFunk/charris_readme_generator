67f203391d810ab659cc0898c272ddb9e3471ec4
Chris Harris <cHarris505@gmail.com>
2023-07-05 21:08:39 -0600

    Updated readMeStyles to include styling for each question. ReadMe.md file is throwing an error with the table of contents values that are preventing the rest of the data from displaying.

--- Styles/readMeStyles.js
+++ Styles/readMeStyles.js
@@ -2,7 +2,16 @@
 function markdownStyles(data) {
     return `
         # ${data.title}
-        ## Description ${data.description}
+        ## INSTALLATION ${data.description}
+        ## TABLE OF CONTENTS ${data.toc}
+        ## INSTALLATION ${data.installation}
+        ## USAGE ${data.usage}
+        ## CREDITS ${data.credits}
+        ## LICENSE ${data.license}
+        ## BADGES ${data.badges}
+        ## FEATURES ${data.features}
+        ## HOW TO CONTRIBUTE ${data.contribute}
+        ## TESTS ${data.tests}
     `;
 };
 
